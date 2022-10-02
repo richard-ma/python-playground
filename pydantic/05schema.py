a06989c4e05ee0a5df0343016c81d67993bf52d9
richard-ma <richard.ma.19850509@gmail.com>
2022-10-02 20:05:11 +0800

    add schema

--- pydantic/05schema.py
+++ pydantic/05schema.py
@@ -1,2 +1,18 @@
 # Pydantic允许直接从对象创建JSON schema
-from pydantic import BaseModel
+from typing import Union
+from pydantic import BaseModel, Field, schema_json_of  # type: ignore
+from typing_extensions import Annotated, Literal # Literal not in typing package
+
+class Cat(BaseModel):
+    pet_type: Literal['cat']
+    cat_name: str
+
+
+class Dog(BaseModel):
+    pet_type: Literal['dog']
+    dog_name: str
+
+
+Pet = Annotated[Union[Cat, Dog], Field(discriminator='pet_type')]
+
+print(schema_json_of(Pet, title='The Pet Schema', indent=2))

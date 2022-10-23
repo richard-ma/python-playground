81908faa370b3915add16d0b9e9e24d2f20e2862
richard-ma <richard.ma.19850509@gmail.com>
2022-10-23 16:18:26 +0800

    add test class

---
+++ pytest/test_class.py
@@ -0,0 +1,11 @@
+from re import X
+
+
+class TestClass():
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, "check") is False
